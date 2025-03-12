24c6954b7ff57790a5027a1b652d342d39132de2
freelancerjoynal <developerjoynal@gmail.com>
2025-03-12 15:50:34 +0600

    role column added to db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,25 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@quickbank.com',
+            'password' => Hash::make('admin123'), // Use Hash::make to hash the password
+            'role' => 1,
+        ]);
+
+        User::create([
+            'name' => 'Manager',
+            'email' => 'manager@quickbank.com',
+            'password' => Hash::make('manager123'), // Use Hash::make here as well
+            'role' => 2,
+        ]);
+
+        User::create([
+            'name' => 'User',
+            'email' => 'user@quickbank.com',
+            'password' => Hash::make('user123'), // And here
+            'role' => 0,
         ]);
     }
 }
